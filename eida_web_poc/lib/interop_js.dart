58f342f64e05db914d9209c2420e71a05aec680a
loay-albarmawi <151910061+loay-albarmawi@users.noreply.github.com>
2024-05-27 11:04:17 +0300

    added toolkits and init function

---
+++ eida_web_poc/lib/interop_js.dart
@@ -0,0 +1,41 @@
+import 'dart:js' as js;
+
+class InteropJS {
+  void initialize() {
+    js.context.callMethod('eval', [
+      '''
+
+      var script = document.createElement('script');
+      var script = document.createElement('script');
+      script.src = 'assets/toolkit_sample.js';
+      script.onload = function() {
+        console.log('Script loaded');
+        Initialize();
+      };
+      document.body.appendChild(script);
+
+      var script2 = document.createElement('script');
+      script2.src = 'assets/eidatoolkit.js';
+      script2.onload = function() {
+        console.log('Another script loaded');
+        // Additional code to execute after the script is loaded
+      };
+      document.body.appendChild(script2);
+      script.src = 'assets/toolkit_sample.js';
+      script.onload = function() {
+        console.log('Script loaded');
+        Initialize();
+      };
+      document.body.appendChild(script);
+    '''
+    ]);
+  }
+
+  void registerDevice(String deviceId) {
+    js.context.callMethod('registerDevice', [deviceId]);
+  }
+
+  String scanID() {
+    return js.context.callMethod('scanID');
+  }
+}
