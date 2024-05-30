da79b0cc4ac165da2dc6f498670a8f43f620eace
loay-albarmawi <151910061+loay-albarmawi@users.noreply.github.com>
2024-05-30 11:47:04 +0300

    .

--- eida_web_poc/lib/interop_js.dart
+++ eida_web_poc/lib/interop_js.dart
@@ -7,19 +7,12 @@ class InteropJS {
 
 
 
-      var script2 = document.createElement('script');
-      script2.src = 'assets/eidatoolkit.js';
-      script2.onload = function() {
-        console.log('Another script loaded');
-        // Additional code to execute after the script is loaded
-      };
-            var script = document.createElement('script');
       var script = document.createElement('script');
-      script.src = 'assets/toolkit_sample.js';
-            document.body.appendChild(script2);
-
+      script.src = 'assets/eidatoolkit.js';
+    
+       
       document.body.appendChild(script);
-      script.src = 'assets/toolkit_sample.js';
+     
       script.onload = function() {
         console.log('Script loaded');
         Initialize();
